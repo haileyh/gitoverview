7d9194f355119f64615ec20a19fbf4d9dd315041
Hailey Henkel <haileyhenkel@wireless-10-146-176-93.public.utexas.edu>
2013-09-23 18:56:52 -0500

    Added practice.java

---
+++ practice.java
@@ -0,0 +1,12 @@
+public class practice
+{
+	public static void main(String[] args) {
+		int count = 0;
+		for(int i = -100; i < 100; i++) {
+			count++;
+			System.out.println("count: " + count);
+			System.out.println(i + " squared = " + (i * i));
+		}
+		
+	}
+}
